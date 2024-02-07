3687194b024d07894b62f45e62e8962a7c31367e
f-forcher <francesco.forcher@gmail.com>
2024-02-07 14:13:50 +0100

    wip map

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use anyhow::{anyhow, Context, Result};
 use regex::Regex;
+use std::collections::HashMap;
 use std::time::SystemTime;
+use tokio::sync::RwLock;
 use tonic::{Request, Response, Status};
 
 use api::grpc::message_storage;
@@ -11,17 +13,23 @@ pub mod api;
 #[cfg(test)]
 mod tests;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct KeyAndTenant {
     key: String,
     tenant: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct IdAndCount {
+    pub id: u64,
+    pub count: u64,
 }
 
 impl KeyAndTenant {
     pub fn try_from_parts(key: &str, tenant: &str) -> Result<Self> {
         let valid_key_pattern = r"^K-[a-z0-9]{5}-[A-Z]$";
         let valid_key_regex = Regex::new(valid_key_pattern)
-        .with_context(|| format!("Wrong key regex: {valid_key_pattern}"))?;
+            .with_context(|| format!("Wrong key regex: {valid_key_pattern}"))?;
 
         if !valid_key_regex.is_match(key) {
             return Err(anyhow!("Key is wrong: {}", key));
@@ -43,7 +51,9 @@ impl KeyAndTenant {
 }
 
 #[derive(Debug, Default)]
-pub struct MessageStorageService {}
+pub struct MessageStorageService {
+    message_store: RwLock<HashMap<KeyAndTenant, IdAndCount>>,
+}
 
 #[tonic::async_trait]
 impl MessageStorage for MessageStorageService {
@@ -62,13 +72,26 @@ impl MessageStorage for MessageStorageService {
                 .map_err(|e| Status::internal(format!("Internal error: {e}")))?;
 
         let request = request.into_inner();
-        let _key_and_tenant = KeyAndTenant::try_from_parts(&request.key, &request.tenant)
+        let key_and_tenant = KeyAndTenant::try_from_parts(&request.key, &request.tenant)
             .map_err(|e| Status::invalid_argument(format!("{e}")))?;
+
+        let id_and_count = self
+            .message_store
+            .read()
+            .await
+            .get(&key_and_tenant)
+            .unwrap_or(&IdAndCount {
+                id: self.message_store.read().await.len() as u64 + 1,
+                count: 0,
+            });
+        let is_new = id_and_count.count == 0;
+        self.message_store
+            .insert(key_and_tenant, how_many_already + 1);
 
         let reply = MessageResponse {
             timestamp: Some(prost_types::Timestamp::from(now)),
-            id: 1,     //TODO: Implement a proper ID
-            new: true, // TODO: Implement new flag
+            id: 1,       //TODO: Implement a proper ID
+            new: is_new, // TODO: Implement new flag
         };
 
         Ok(Response::new(reply)) // Send back our formatted greeting
